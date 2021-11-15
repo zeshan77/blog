7544f916907bede25639b7003963b983c84022d0
Zeshan Khattak <zeshan77@gmail.com>
2021-11-15 13:02:28 +0500

    Table styling

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -9,34 +9,33 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <table class="table-auto">
+                    <table class="table-auto w-full border border-collapse">
                         <thead>
                             <tr>
-                                <th>ID</th>
-                                <th>Slug</th>
-                                <th>Title</th>
-                                <th>Has published</th>
-                                <th>Created On</th>
-                                <th>Actions</th>
+                                <th class="border">ID</th>
+                                <th class="border">Slug</th>
+                                <th class="border">Title</th>
+                                <th class="border">Has published</th>
+                                <th class="border">Created On</th>
+                                <th class="border">Actions</th>
                             </tr>
                         </thead>
                         <tbody>
                             @forelse($posts as $post)
-                                <tr>
-                                    <td>{{ $post->id }}</td>
-                                    <td>{{ $post->slug }}</td>
-                                    <td>{{ $post->title }}</td>
-                                    <td>{{ $post->has_published }}</td>
-                                    <td>{{ $post->created_at }}</td>
-                                    <td>--</td>
+                                <tr class="py-5">
+                                    <td class="border py-3">{{ $post->id }}</td>
+                                    <td class="border py-3">{{ $post->slug }}</td>
+                                    <td class="border py-3">{{ $post->title }}</td>
+                                    <td class="border py-3">{{ $post->has_published }}</td>
+                                    <td class="border py-3">{{ $post->created_at }}</td>
+                                    <td class="border py-3">--</td>
                                 </tr>
                             @empty
-                                <tr>
-                                    <td colspan="6">No posts found in the database.</td>
+                                <tr class="py-4">
+                                    <td colspan="6" class="text-center py-3 border">No posts found in the database.</td>
                                 </tr>
                             @endforelse
                         </tbody>
-                        <tfoot></tfoot>
                     </table>
                 </div>
             </div>
